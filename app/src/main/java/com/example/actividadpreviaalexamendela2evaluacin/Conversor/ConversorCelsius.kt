148baf8bde4680f9132a7e016dfd1f1fe94e98b9
aludam2 <german.escudero1@educa.madrid.org>
2025-02-10 12:32:17 +0100

    a

---
+++ app/src/main/java/com/example/actividadpreviaalexamendela2evaluacin/Conversor/ConversorCelsius.kt
@@ -0,0 +1,4 @@
+package com.example.actividadpreviaalexamendela2evaluacin.Conversor
+
+class ConversorCelsius {
+}
--- app/src/main/java/com/example/actividadpreviaalexamendela2evaluacin/MainActivity.kt
+++ app/src/main/java/com/example/actividadpreviaalexamendela2evaluacin/MainActivity.kt
@@ -6,12 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.actividadpreviaalexamendela2evaluacin.ui.theme.ActividadPreviaAlExamenDeLa2ªEvaluaciónTheme
+
+/*
+Presencia de la empresa en el mundo (ciudad y país)
+● Madrid - España
+● París - Francia
+● Londres - Reino Unido
+● Porto Alegre - Brasil
+● Acapulco - México
+● Vancouver - Canadá
+● Houston - Estados Unidos de América
+● Casablanca - Marruecos
+● Osaka - Japón
+● Melbourne - Australia
+● Ankara - Turquía
+● Dubai - Emiratos Árabes Unidos
+ */
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +37,6 @@ class MainActivity : ComponentActivity() {
             ActividadPreviaAlExamenDeLa2ªEvaluaciónTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +46,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    LazyColumn {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ActividadPreviaAlExamenDeLa2ªEvaluaciónTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
